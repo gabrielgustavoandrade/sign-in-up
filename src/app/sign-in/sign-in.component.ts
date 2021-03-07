a69545c79ea3fde0589d2bfe11a8907647c22eaa
Gabriel Andrade <gabrielgustavoandrade@gmail.com>
2021-03-07 19:11:54 -0300

    form validator

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
+  signInForm: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.signInForm = this.formBuilder.group({
+      password: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    console.warn('Your order has been submitted', this.signInForm.value);
+    this.signInForm.reset();
+  }
 }
